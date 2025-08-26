7622c035199e02b914fe0997b228ab24d8957259
Narain008 <narain8567.beaift25@chitkara.edu.in>
2025-08-26 11:13:26 +0530

    Add files via upload

---
+++ python.py/abc_1.py
@@ -0,0 +1,18 @@
+a=int(input("Enter 1 no."))
+b=int(input("Enter 2 no."))
+c=a+b
+addition=c
+print("On addition answer is=",c)
+d=a-b
+subtraction=d
+if d>=0:
+    print("On subtraction answer will be positive A is greater than B=",d)
+else:
+    print("On subtraction Answer will be negative as B is greater than A=",d)
+e=a*b
+if e==0:
+ print("Either A or B one is zero")
+else:
+   print("On multiplication Answer is=",e)
+f=a/b
+print("On dividing A by B answer is=",f)
---
+++ python.py/square.py
@@ -0,0 +1,5 @@
+i=0
+for i in range(1,11):
+    k=i*i
+    print(k)
+    i+=1
