360a9d8297be77d3630262397df5bb9afa0ca46f
Narain008 <narain8567.beaift25@chitkara.edu.in>
2025-08-28 11:12:41 +0530

    Add files via upload

---
+++ how to print pattern.py
@@ -0,0 +1,34 @@
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i*j,end="")
+#     print()
+
+
+
+# row=5
+# for i in range(1,row+1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+# row=5
+# for i in range(1,row+1):
+#     for j in range(i):
+#         print(i+1,end=" ")
+#     print()
+
+
+# row=5
+# column=1
+# for i in range(1,row+1):
+#     for j in range(i):
+#         print(column,end=" ")
+#         column+=1
+#     print()
+
+num=7
+for i in range(1,6):
+    for j in range(num-1):
+        print(num,end=" ")
+    print()
